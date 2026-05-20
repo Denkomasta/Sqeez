94ace7a3f266b8afc1abce5cdedff00de4294509
Denny Valach <xvalach3@fi.muni.cz>
2026-05-20 21:45:49 +0200

    docs: constants

--- src/backend/Sqeez.Api/Constants/QuizConstants.cs
+++ src/backend/Sqeez.Api/Constants/QuizConstants.cs
@@ -1,9 +1,13 @@
 ﻿namespace Sqeez.Api.Constants
 {
+    /// <summary>
+    /// Shared quiz structure limits used by quiz question and option validation.
+    /// </summary>
     public static class QuizConstants
     {
+        /// <summary>
+        /// Maximum number of options that can be attached to one quiz question.
+        /// </summary>
         public const int MaxOptionsPerQuestion = 8;
-        // public const int MaxTitleLength = 100;
-        // public const int DefaultTimeLimitSeconds = 30;
     }
 }
